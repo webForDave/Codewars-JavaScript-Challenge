fae0c5df9be0ab67b298abf61fc3fe2d45eaff39
Lucky Yoila <83231040+LuckyYoila@users.noreply.github.com>
2023-05-24 22:44:46 +0100

    Update app.js
    Solution for checking pin containing letters

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 let validatePin = pin => {
-    let numberAsString = pin.toString();
-    
-    if (numberAsString.length === 4 || numberAsString.length === 6) {
-        return true;
-
-    }   else if (numberAsString.length === 5) {
-        return false;
-
-    }   else if (numberAsString.length < 4 || numberAsString.length > 6) {
-        return false;
-
-     }
-
+  
+//     Simply check if the length is 4 or 6, and it has to be divisible a number. (strings with letters will not be divisible) 
+//     if it is not divisible (it returns NaN which is a falsey value) the condition will not be met and return false
+    if ((pin.length === 4 || pin.length === 6) && (pin/1)) return true;
+    else return false
 }
 console.log(validatePin('123456'));
