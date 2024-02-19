0526cdf78bd045916a1467b20d041467689aab92
David <dakinola54@gmail.com>
2024-02-18 22:18:53 -0800

    exercise

--- WEEK 1/week1.js
+++ WEEK 1/week1.js
@@ -2,3 +2,17 @@ const abbrevator = (word1, word2) => {
     return(`${word1[0]}.${word2[0]}`);
 }
 console.log(abbrevator('David', 'Akinola'))
+
+
+
+
+//////////////SECOND/////////////
+let greeting = (name, owner) => {
+    if (name === owner) {
+        return('Hello boss');
+
+    } else if (name != owner) {
+        return('Hello guest');
+    }
+}
+console.log(greeting('David', 'John'));
